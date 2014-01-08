34bf5acfa98978b85f9cc792c8c5f7d330347e51
jcrossler <jay.crossler@gmail.com>
2014-01-07 21:46:12 -0500

    Update default password to 'geoevents'

--- geoevents/settings.py
+++ geoevents/settings.py
@@ -16,11 +16,11 @@ COMPRESS_ENABLED = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geoevents_app', # Or path to database file if using sqlite3.
-        'USER': 'geoevents_app', # Not used with sqlite3.
-        'PASSWORD': 'fixme', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'geoevents_app', 
+        'USER': 'geoevents_app',
+        'PASSWORD': 'geoevents', 
+        'HOST': '', # Set to empty string for localhost.
+        'PORT': '', # Set to empty string for default. 
     }
 }
 
