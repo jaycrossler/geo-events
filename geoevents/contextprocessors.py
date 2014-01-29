9c223752770dc5709217813f43b3642fe5410824
jcrossler <jay.crossler@gmail.com>
2014-01-29 14:29:08 -0500

    Added required DFARS Rights statement

--- geoevents/contextprocessors.py
+++ geoevents/contextprocessors.py
@@ -1,3 +1,6 @@
+# This technical data was produced for the U. S. Government under Contract No. W15P7T-13-C-F600, and
+# is subject to the Rights in Technical Data-Noncommercial Items clause at DFARS 252.227-7013 (FEB 2012)
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 from geoevents.version import get_git_changeset, get_version
